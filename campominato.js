3bc1fb86a56a0a6d904a53025d089fdcdd5fe754
Starliege <urrianif@gmail.com>
2022-07-29 15:54:30 +0200

    js done devo solo ordinare css griglia

--- campominato.js
+++ campominato.js
@@ -8,3 +8,56 @@
 // con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 // con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 
+// variabili
+const griglia = document.getElementById("griglia");
+let cella = document.getElementsByClassName("cella");
+const diffSet = document.getElementById("select");
+
+
+// difficoltà
+document.getElementById("facile").addEventListener("click", diffFacile);
+document.getElementById("medio").addEventListener("click", diffMedio);
+document.getElementById("difficile").addEventListener("click", diffDifficile);
+
+// funzioni
+function diffFacile() {
+  diffSet.classList.add("hidden");
+  griglia.innerHTML = "";
+  griglia.classList.remove("animazione", "medio", "difficile");
+  griglia.classList.add("animazione", "facile");
+  for (i=1; i<101; i++) {
+    griglia.innerHTML += `<div class="cella">${i}</div>`;
+  }
+  cellaAzzurra();
+}
+
+function diffMedio() {
+  diffSet.classList.add("hidden");
+  griglia.innerHTML = "";
+  griglia.classList.remove("animazione", "facile", "difficile");
+  griglia.classList.add("animazione", "medio");
+  for (i=1; i<82; i++) {
+    griglia.innerHTML += `<div class="cella">${i}</div>`;
+  }
+  cellaAzzurra();
+}
+
+function diffDifficile() {
+  diffSet.classList.add("hidden");
+  griglia.innerHTML = "";
+  griglia.classList.remove("animazione", "facile", "medio");
+  griglia.classList.add("animazione", "difficile");
+  for (i=1; i<50; i++) {
+    griglia.innerHTML += `<div class="cella">${i}</div>`;
+  }
+  cellaAzzurra();
+}
+
+// click cella
+function cellaAzzurra() {
+  for (i=0; i<cella.length; i++) {
+    cella[i].addEventListener("click", function(){
+      this.classList.add("azzurro");
+    });
+  }
+}
